d3594706408aaaf500c6a6c7e72b21cb12ea481e
Cassio Furlan <Cassio.Furlan@joysonsafety.com>
2021-07-14 11:31:12 -0300

    Course finished

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,16 @@
 def principal():
    import random
-#   print("Keep it logically awesome.")
+   print("Keep it logically awesome.")
 
    f = open("quotes.txt")
    quotes = f.readlines()
    f.close()
 
-   last = 13
+   last = 16
    rnd = random.randint(0, last)
-   print(quotes[rnd])
+   rnd2 = random.randint(0, last)
+   print(quotes[rnd],end="")
+   print(quotes[rnd2],end="")
 
 if __name__== "__main__":
    principal()
